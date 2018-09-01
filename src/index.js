18bbfbf0636c36607b984cee4a25335555da3a15
htao <htao@thoughtworks.com>
2018-09-01 16:23:07 +0800

    [taohui] calculate winer

--- src/index.js
+++ src/index.js
@@ -20,6 +20,26 @@ class Board extends React.Component {
     };
   }
 
+ calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
   handleClick(i) {
     const squares = this.state.squares.slice();
     squares[i] = this.state.xIsNext ? 'X': 'O';
@@ -40,7 +60,13 @@ class Board extends React.Component {
   }
 
   render() {
-    let status = 'Next player is: ' + (this.state.xIsNext ? 'X' : 'O');
+    const winner = this.calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
